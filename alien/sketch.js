fd286c905815e7025920aa17d0e10ee66c0e05a9
hlmasson <hmasson@iu.edu>
2026-04-01 20:19:42 +0000

    commit1

--- alien/sketch.js
+++ alien/sketch.js
@@ -10,12 +10,12 @@ function draw() {
   textStyle(BOLDITALIC)
   textFont('Times New Roman')
   fill('#00DD1C')
-  textSize(60)
-  text("Welcome to my home page",windowWidth/2,300)
+  textSize(45)
+  text("Welcome to my home page",windowWidth/2,350)
   
-  a.style('color','#00DD1C')
-  a.attribute('align', 'center')
-  a.style('font-size', '40px')
-  a.style('font-weight','bold')
-  a.position(windowWidth/2-40, 450)
+  a.style('color','#00DD1C');
+  a.attribute('align', 'center');
+  a.style('font-size', '30px');
+  a.style('font-weight','bold');
+  a.position(windowWidth/2-40, 500)
 }
