5c7f254f319c416412b96f614f3d77f738cb664f
hlmasson <hmasson@iu.edu>
2026-04-06 19:15:10 -0400

    index fix

--- sketch.js
+++ sketch.js
@@ -11,11 +11,11 @@ function draw() {
   textFont('Times New Roman')
   fill('#00DD1C')
   textSize(45)
-  text("Welcome to my home page",windowWidth/2,350)
+  text("Welcome to my home page",windowWidth/2-50,250)
   
   a.style('color','#00DD1C');
   a.attribute('align', 'center');
   a.style('font-size', '30px');
   a.style('font-weight','bold');
-  a.position(windowWidth/2-40, 500)
+  a.position(windowWidth/2-40, 400)
 }
