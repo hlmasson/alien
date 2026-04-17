8904ed336698615d14bef2e5a46f758fc9ff072e
hlmasson <hmasson@iu.edu>
2026-04-17 15:45:54 -0400

    password

--- docspage.js
+++ docspage.js
@@ -5,25 +5,31 @@ let myInput;
 let showText;
 
 function preload() {
-a = createA('libraries/ALIEN ZINE.pdf', 'DOCUMENT 1 TRANSLATED','_blank');
+ a = createA('https://geocities.restorativland.org/', 'DOCUMENT 1 TRANSLATED','_blank');
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
+  background('#ffffff');
   myInput = createInput('passkey');
   myInput.position(110, 425);
   myInput.changed(checkText);
+   a.style('color','#00DD1C');
+   a.style('background-color','white' ); 
+    a.attribute('align', 'left');
+    a.style('font-size', '18px');
+    a.position(110, 450);
+    
 }
 
 function draw() {
 fill("#00DD1C")
 textFont("Times New Roman");
-textSize(18);
-    if (showText) {
-        text("text", 110,50);
-        a.style('color','#00DD1C');
-        a.position(110,450);
-    }
+textSize(20);
+if (showText) {
+    text("text", 110,50);
+   
+}
 }
 
 function checkText() {
